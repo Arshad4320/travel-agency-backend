7915d5f285ee8778acb2245a0aec5e19e4d05301
Arshad Ullah <mdarshadullah835@gmail.com>
2025-08-31 14:11:23 +0600

    project setup done

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mern-backend";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ DB Connection Failed:", err);
+  });
