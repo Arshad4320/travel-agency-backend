e1f0899c1d46636cea0d4833d805562a80bfb628
Arshad Ullah <mdarshadullah835@gmail.com>
2025-09-01 22:05:45 +0600

    some change

--- src/module/bus/bus.service.ts
+++ src/module/bus/bus.service.ts
@@ -10,7 +10,48 @@ const createBus = async (payload: IBus) => {
   }
 }
 
-const findBus = async () => {}
+const findBus = async (filters: any = {}) => {
+  try {
+    const {
+      maxPrice,
+      minPrice,
+      limit = 10,
+      page = 1,
+      search,
+      from,
+      to,
+    } = filters
+    const query: any = {}
+    if (search) {
+      query.$or = [
+        { name: { $regex: 'search', options: 'i' } },
+        { from: { $regex: 'search', options: 'i' } },
+        { to: { $regex: 'search', options: 'i' } },
+      ]
+    }
+    if (from) query.from = from
+    if (to) query.to = to
+    if (minPrice || maxPrice) {
+      query.price = {}
+      if (minPrice) query.price.$gte = Number(minPrice)
+      if (maxPrice) query.price.$lte = Number(maxPrice)
+    }
+    const skip = Number(page - 1) * Number(limit)
+    const result = await BusModel.find(query).skip(skip).limit(Number(limit))
+    const total = await BusModel.countDocuments(query)
+    return {
+      result,
+      meta: {
+        total,
+        page: Number(page),
+        limit: Number(limit),
+        totalPages: Math.ceil(total / Number(limit)),
+      },
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
 const findByBusId = async (id: string) => {
   try {
     const result = await BusModel.findById(id)
