5831e87fd47c711fca1b50c135aa832f5d361cb1
Md Arshad Ullah <84790948+Arshad4320@users.noreply.github.com>
2025-09-13 13:06:27 +0600

    Update transport.model.ts

--- src/module/transport/transport.model.ts
+++ src/module/transport/transport.model.ts
@@ -3,13 +3,17 @@ import { ITransport } from './transport.interface'
 
 const TransportSchema = new Schema<ITransport>(
   {
-    flightNumber: { type: String, required: true },
+    transportNumber: { type: String, required: true },   
     providerName: { type: String, required: true },
-    planeImage: { type: String },
-    airlineName: { type: String, required: true },
+    transportImage: { type: String },                   
+    transportName: {                                  
+      type: String,
+      enum: ["Airplane", "Bus", "Ship", "Train"],
+      required: true,
+    },
     from: { type: String, required: true },
     to: { type: String, required: true },
-    departureTime: { type: String, required: true },
+    departureTime: { type: String, required: true },    
     arrivalTime: { type: String, required: true },
     durationMinutes: { type: Number },
     price: { type: Number, required: true },
